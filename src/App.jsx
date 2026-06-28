d93a9e4dcdf7bbaa4b487852e2c2a4caf46f076a
Muralikrishnan <muralikrishnan2005p@gmail.com>
2026-06-28 07:14:25 +0530

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Setup from "./pages/Setup";
@@ -6,20 +6,21 @@ import Interview from "./pages/Interview";
 import Analysis from "./pages/Analysis";
 import Report from "./pages/Report";
 
-function App(){
+function App() {
 
-  return(
-    <BrowserRouter>
-    <Routes>
+  return (
+    <HashRouter>
+      <Routes>
 
-      <Route path="/" element={<Home />} />
-      <Route path="/setup" element={<Setup />} />
-      <Route path="/interview" element={<Interview />}/>
-      <Route path="/analysis" element={<Analysis />}/>
-      <Route path="/report"  element={<Report />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/setup" element={<Setup />} />
+        <Route path="/interview" element={<Interview />} />
+        <Route path="/analysis" element={<Analysis />} />
+        <Route path="/report" element={<Report />} />
 
-    </Routes>
-    </BrowserRouter>
-  )
+      </Routes>
+    </HashRouter>
+  );
 }
+
 export default App;
